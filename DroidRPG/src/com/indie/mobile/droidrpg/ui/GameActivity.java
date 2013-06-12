2e29351b1c8ec0960be3c9d9c602a84ca6bcdf0c
Anthony Dwyer <anthonydwyer@gmail.com>
2013-06-12 19:42:10 -0400

    Added menu placeholders for the hotbar and the avatar menus Changed the OnTouchListener for the game board to the Game object.

--- DroidRPG/src/com/indie/mobile/droidrpg/ui/GameActivity.java
+++ DroidRPG/src/com/indie/mobile/droidrpg/ui/GameActivity.java
@@ -1,15 +1,20 @@
 package com.indie.mobile.droidrpg.ui;
 
 import com.indie.mobile.droidrpg.R;
+import com.indie.mobile.droidrpg.game.Game;
+
 import android.os.Bundle;
 import android.app.Activity;
 
-public class GameActivity extends Activity {
-
+public class GameActivity extends Activity {	
+	private Game _game;	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this._game = Game.getGameInstance();
+		this._game.loadConfiguration();
 		setContentView(R.layout.activity_game);
-		
+		this.findViewById(R.id.gameboard).setOnTouchListener(this._game);
 	}
 }
